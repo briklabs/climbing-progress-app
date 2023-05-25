ba87e6298361f17bb5281d7c611ebe55a374c07a
krystlc <alex.valle@gmail.com>
2023-05-24 23:32:30 -0400

    implemented a routine component

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import HomePage from "./pages/HomePage";
 import ProfilePage from "./pages/ProfilePage";
 import LeaderboardPage from "./pages/LeaderboardPage";
 import ExplorePage from "./pages/ExplorePage";
+import NewRoutinePage from "./pages/NewRoutinePage";
 
 export default function App() {
   return (
@@ -16,6 +17,7 @@ export default function App() {
         <Route path="explore" element={<ExplorePage />} />
         <Route path="board" element={<LeaderboardPage />} />
         <Route path="profile" element={<ProfilePage />} />
+        <Route path="new-routine" element={<NewRoutinePage />} />
       </Route>
       <Route path="/terms" element={<TermsConditionsPage />} />
     </Routes>
--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,5 +1,5 @@
 import { IconType } from "react-icons";
-import { FiHome, FiCompass, FiList, FiUser } from "react-icons/fi";
+import { FiHome, FiCompass, FiList, FiUser, FiPlus } from "react-icons/fi";
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function DashboardPage() {
@@ -28,11 +28,19 @@ export default function DashboardPage() {
   const navClassNames =
     "inline-flex flex-col items-center justify-center gap-1 h-full";
   return (
-    <div className="flex flex-col h-screen w-screen">
+    <div className="flex flex-col h-[100dvh] w-screen">
       <main className="flex-1 overflow-auto">
         <Outlet />
       </main>
-      <nav className="bg-white grid grid-cols-4 h-24">
+      <nav className="bg-white grid grid-cols-4 h-24 relative">
+        <NavLink
+          to="new-routine"
+          end
+          className="absolute w-16 h-16 bg-black text-white flex flex-col items-center justify-center -top-8 right-1/2 -mr-8 rounded-full"
+        >
+          <FiPlus className="w-6 h-6 mx-auto" />
+          <span className="text-xs font-light tracking-wider">Start</span>
+        </NavLink>
         {navLinks.map((n, i) => (
           <NavLink
             to={n.value}
