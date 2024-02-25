d101e9533185e19bd1c3a4786d01249b96d5b164
krystlc <alex.valle@gmail.com>
2024-02-24 19:00:10 -0500

    fix(email): following https://joeczubiak.com/netlify-contact-form-with-react-ant-design/ example

--- src/components/BetaTesterApplicationCTA.tsx
+++ src/components/BetaTesterApplicationCTA.tsx
@@ -1,61 +1,91 @@
 import { Button, Form, Input, Radio } from "antd";
-import { FormEvent } from "react";
+
+function encode(data: any) {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + `=` + encodeURIComponent(data[key]))
+    .join(`&`);
+}
 
 export default function BetaTesterApplicationCTA() {
-  const handleSubmit = async (
-    event: FormEvent<HTMLFormElement>
-  ): Promise<void> => {
-    event.preventDefault();
+  const formName = `beta-tester-application`;
 
-    const myForm = event.target as HTMLFormElement;
-    const formData = new FormData(myForm);
-
-    try {
-      const response = await fetch("/", {
-        method: "POST",
-        headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: new URLSearchParams(formData as any).toString(),
-      });
-
-      if (response.ok) {
-        alert("Thank you for your submission");
-      } else {
-        throw new Error(`Submission failed with status: ${response.status}`);
-      }
-    } catch (error: unknown) {
-      alert((error as Error).message);
+  const handleSubmit = (values: any) => {
+    if (values[`bot-field`] === undefined) {
+      delete values[`bot-field`];
     }
+
+    fetch(`/`, {
+      method: `POST`,
+      headers: { "Content-Type": `application/x-www-form-urlencoded` },
+      body: encode({
+        "form-name": formName,
+        ...values,
+      }),
+    })
+      .then(() => showSuccess())
+      .catch((error) => showError(error));
+  };
+
+  const showSuccess = () => {
+    // TODO: Show a success message or navigate to a success page.
+    console.log(`form submitted successfully`);
+  };
+
+  const showError = (error: any) => {
+    // TODO: Show an error message to the user
+    console.log(`There was an error submitting the form`);
+    console.log(error);
   };
 
   return (
-    <Form
-      layout="horizontal"
-      data-netlify
-      name="beta-tester-application"
-      method="post"
-      onSubmitCapture={handleSubmit}
-    >
-      <input type="hidden" name="form-name" value="beta-tester-application" />
-      <Form.Item label="Radio" required>
-        <Radio.Group>
-          <Radio name="platform" value="ios">
-            iOS
-          </Radio>
-          <Radio name="platform" value="android">
-            Android
-          </Radio>
-        </Radio.Group>
-      </Form.Item>
-      <Form.Item label="Email" required>
-        <Input
-          name="email"
-          type="email"
-          placeholder="janja.garnbret@gmail.com"
-        />
-      </Form.Item>
-      <Form.Item>
-        <Button type="primary">Apply</Button>
-      </Form.Item>
-    </Form>
+    <>
+      <form
+        name={formName}
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        hidden
+      >
+        <input type="radio" name="platform" />
+        <input type="email" name="email" />
+      </form>
+      <Form
+        layout="horizontal"
+        data-netlify
+        name="cf"
+        method="post"
+        onFinish={handleSubmit}
+      >
+        <Form.Item
+          label="Don't fill this out"
+          className={`hidden`}
+          style={{ display: `none` }}
+          name="bot-field"
+        >
+          <Input type={`hidden`} />
+        </Form.Item>
+        <Form.Item label="Radio" required>
+          <Radio.Group>
+            <Radio name="platform" value="ios">
+              iOS
+            </Radio>
+            <Radio name="platform" value="android">
+              Android
+            </Radio>
+          </Radio.Group>
+        </Form.Item>
+        <Form.Item label="Email" required>
+          <Input
+            name="email"
+            type="email"
+            placeholder="janja.garnbret@gmail.com"
+          />
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            Apply
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 }
