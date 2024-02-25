c52f018db371ecd46a276391bcd8cfd50fc67856
krystlc <alex.valle@gmail.com>
2024-02-25 10:58:27 -0500

    feat(email): hide hidden field

--- src/routes/BetaTesterApplicationCTA.svelte
+++ src/routes/BetaTesterApplicationCTA.svelte
@@ -1,5 +1,5 @@
 <form name="beta-tester-application" method="POST" netlify netlify-honeypot="bot-field">
-	<input type="text" name="bot-field" />
+	<input type="text" name="bot-field" hidden />
 	<input type="hidden" name="form-name" value="beta-tester-application" />
 	<input
 		name="email"
